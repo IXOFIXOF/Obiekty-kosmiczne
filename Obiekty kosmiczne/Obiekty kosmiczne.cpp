e841b363b3ea3b049338640c6970ae72d72e2b12
IXOFIXOF <IXOF@POCZTA.ONET.PL>
2018-05-22 23:29:09 +0200

    Init commit

---
+++ Obiekty kosmiczne/Obiekty kosmiczne.cpp
@@ -0,0 +1,13 @@
+// Obiekty kosmiczne.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Klasy bazowe obiekty kosmiczne, klasy pochodne gwiazdy, kwazary, galaktyki, czarne dziury, konstelacje, gwiazdozbiory. Przeci¹¿yæ operator strumienia i dodawania i odejmowania obiektów z gwiazdozbiorów oraz parê operatorów wed³ug w³asnego pomys³u.\n";
+	std::cin.get();
+    return 0;
+}
+
